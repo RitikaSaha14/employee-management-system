794f1e285ae1348f5abfca62c0cc2b7355b7a5b8
RITIKA SAHA <95564532+RitikaSaha14@users.noreply.github.com>
2025-12-18 20:02:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const employeeRoutes = require("./routes/employeeRoutes");
+app.use("/employees", employeeRoutes);
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
 
