b589eea7225b1b057a5df700ac1b537c5c00bfb4
RITIKA SAHA <95564532+RitikaSaha14@users.noreply.github.com>
2025-12-18 20:01:57 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,16 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "your_password",
+    database: "employee_db"
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log("MySQL Connected");
+});
+
+module.exports = db;
 
