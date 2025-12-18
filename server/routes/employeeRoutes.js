4094e6cf5832f27b69d18c1e80bdbe20b2dc29cd
RITIKA SAHA <95564532+RitikaSaha14@users.noreply.github.com>
2025-12-18 20:01:25 +0530

    Update employeeRoutes.js

--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -1,1 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+// GET all employees
+router.get("/", (req, res) => {
+    db.query("SELECT * FROM employees", (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+// ADD employee
+router.post("/", (req, res) => {
+    const { name, role, salary } = req.body;
+    const sql = "INSERT INTO employees (name, role, salary) VALUES (?, ?, ?)";
+    db.query(sql, [name, role, salary], err => {
+        if (err) throw err;
+        res.send("Employee Added");
+    });
+});
+
+// DELETE employee
+router.delete("/:id", (req, res) => {
+    const sql = "DELETE FROM employees WHERE id=?";
+    db.query(sql, [req.params.id], err => {
+        if (err) throw err;
+        res.send("Employee Deleted");
+    });
+});
+
+module.exports = router;
+
 
